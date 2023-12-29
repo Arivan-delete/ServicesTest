5b51b728032bd0120454a3c12c8f112915af0250
Artem <artem.mamedov5375@gmail.com>
2023-12-29 23:26:52 +0200

    Job Service and Job Scheduler

---
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -0,0 +1,56 @@
+package com.example.servicestest
+
+import android.app.Service
+import android.app.Service.START_REDELIVER_INTENT
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutineScope.launch {
+            for (i in 0..10) {
+                delay(1000)
+                log("Timer $i")
+            }
+            jobFinished(p0, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    companion object {
+        const val JOB_ID = 100
+    }
+}
